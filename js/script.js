d254f05cfc2bbf4fee00ef20b493b71c2273973b
Nick Robin <nick.robin@braze.com>
2019-01-19 12:27:26 -0500

    meets expectations

--- js/script.js
+++ js/script.js
@@ -1,21 +1,127 @@
-//make sure everything is supportive of non js browsers
+const $name = $('#name');
+const $email = $('#email');
+const $title = $('#other-title');
+const $registration = $('.activities input');
+const $cc_num = $('#cc-num');
+const $zip = $('#zip');
+const $cvv = $('#cvv');
+const $all = $('input[name="all"]');
+const $js_frameworks = $('input[name="js-frameworks"]');
+const $js_libs = $('input[name="js-libs"]');
+const $express = $('input[name="express"]');
+const $node = $('input[name="node"]');
+const $build_tools = $('input[name="build-tools"]');
+const $npm = $('input[name="npm"]');
+
+let totalCost;
 
 $('#other-title').hide().prev().hide();
-
+$('#colors-js-puns').hide();
 $('#credit-card').hide();
 $('#paypal').hide();
 $('#bitcoin').hide();
+$('.activities').append(`<span> Total Registration Cost: $0 </span>`);
+
+//individual functions for validation of each required field
+function isValidName(name) {
+
+  if (name.val() != "") {
+    name.removeClass('invalid-input');
+    name.prev().removeClass('invalid-label')
+    return true;
+  } else {
+    name.addClass('invalid-input');
+    name.prev().addClass('invalid-label')
+    return false;
+  }
+}
+
+function isValidEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.val())) {
+    email.removeClass('invalid-input');
+    email.prev().removeClass('invalid-label');
+    return true;
+  } else {
+    email.addClass('invalid-input');
+    email.prev().addClass('invalid-label');
+    return false;
+  }
+}
+
+function isValidTitle(title) {
+  if ($('#other-title').val() != "") {
+    title.removeClass('invalid-input');
+    title.prev().removeClass('invalid-label');
+    return true;
+  } else {
+    title.addClass('invalid-input');
+    title.prev().addClass('invalid-label');
+    return false;
+  }
+}
+
+function isValidRegistration(registration) {
+
+  if ($registration.is(':checked')) {
+    $('#register-legend').removeClass('invalid-legend');
+    return true;
+  } else {
+    $('#register-legend').addClass('invalid-legend');
+  }
+}
+
+function isValidCreditCard(cc_num, zip, cvv) {
+
+  let isValidCC = true;
+  let isValidZip = true;
+  let isValidCvv = true;
+
+  if (/(^\d{13,16}$)/.test(cc_num.val())) {
+    cc_num.removeClass('invalid-input');
+    cc_num.prev().removeClass('invalid-label');
+    isValidCC = true;
+  } else {
+    cc_num.addClass('invalid-input');
+    cc_num.prev().addClass('invalid-label');
+    isValidCC = false;
+  }
+
+  if (/^\d{5}$/.test(zip.val())) {
+    zip.removeClass('invalid-input');
+    zip.prev().removeClass('invalid-label')
+    isValidZip = true;
+  } else {
+    zip.addClass('invalid-input');
+    zip.prev().addClass('invalid-label')
+    isValidZip = false;
+  }
+
+  if (/^\d{3}$/.test(cvv.val())) {
+    cvv.removeClass('invalid-input');
+    cvv.prev().removeClass('invalid-label')
+    isValidCvv = true;
+  } else {
+    cvv.addClass('invalid-input');
+    cvv.prev().addClass('invalid-label')
+    isValidCvv = false;
+  }
+  return (isValidCC && isValidCvv && isValidZip);
+}
+
+//help calculate the total for classes
+function getRegistrationCost(registration) {
+  let totalRegistrationCost = 0;
+  registration.each(function() {
+    if ($(this).is(':checked')) {
+      totalRegistrationCost += $(this).data('cost');
+    }
+  });
+  return totalRegistrationCost;
+}
 
 
 
-/* on job role change, show text box for other jobs
-
-Give the field an id of “other-title,” and add the placeholder text of "Your Job Role".
-
-Note: You'll need to add the "Other" job role input directly into the HTML and hide it initially with JS in order to get this feature to work when JS is disabled, which is a requirement below.
-*/
-
-//on change of payment drop down, show the relevant fields below
+//show the other title text box when user chooses other
 $('#title').change((e) => {
   if (e.target.value === "other") {
     $('#other-title').show().prev().show();
@@ -25,17 +131,8 @@ $('#title').change((e) => {
 });
 
 
-/*
-”T-Shirt Info” section
-
-For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
-If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
-If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
-When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
-*/
-
-$('#colors-js-puns').hide();
-
+//show / hide t-shirt options based on the design choice
+// there has to be a better way to do this by adding data-attribute to the difference selectors
 $('#design').change((e) => {
 
   if (e.target.value === "js puns") {
@@ -64,40 +161,38 @@ $('#design').change((e) => {
 });
 
 
+//when checking different classes, add the total and prevent users from enrolling in two classes at same time
+$registration.change(function() {
 
-/*
-”Register for Activities” section
+  //first update the total cost
+  totalCost = getRegistrationCost($registration);
+  $('.activities span').replaceWith(`<span> Total Registration Cost: $` + totalCost + `</span>`);
 
-Some events are at the same day and time as others. If the user selects a workshop, don't allow selection of a workshop at the same day and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
-As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.
-*/
-
-
-
-$(':checkbox').change((e) =>
-{
-  if (e.target.checked)
-  { console.log('check status changed to true'); }
+  //then disable out the stuff that conflicts
+  if (this.name === "js-frameworks" && $(this).is(':checked')) {
+    $express.attr("disabled", true);
+  } else if (this.name === "js-frameworks" && $(this).is(':not(:checked)')) {
+    $express.attr("disabled", false);
+  } else if (this.name === "express" && $(this).is(':checked')) {
+    $js_frameworks.attr("disabled", true);
+  } else if (this.name === "express" && $(this).is(':not(:checked)')) {
+    $js_frameworks.attr("disabled", false);
+  } else if (this.name === "js-libs" && $(this).is(':checked')) {
+    $node.attr("disabled", true);
+  } else if (this.name === "js-libs" && $(this).is(':not(:checked)')) {
+    $node.attr("disabled", false);
+  } else if (this.name === "node" && $(this).is(':checked')) {
+    $js_libs.attr("disabled", true);
+  } else if (this.name === "node" && $(this).is(':not(:checked)')) {
+    $js_libs.attr("disabled", false);
+  }
 });
 
 
 
-
-/*
-"Payment Info" section
-
-Display payment sections based on the payment option chosen in the select menu.
-The "Credit Card" payment option should be selected by default. Display the #credit-card div, and hide the "PayPal" and "Bitcoin" information. Payment option in the select menu should match the payment option displayed on the page.
-When a user selects the "PayPal" payment option, the PayPal information should display, and the credit card and “Bitcoin” information should be hidden.
-When a user selects the "Bitcoin" payment option, the Bitcoin information should display, and the credit card and “PayPal” information should be hidden.
-NOTE: The user should not be able to select the "Select Payment Method" option from the payment select menu, because the user should not be able to submit the form without a chosen payment option.
-*/
-
 //on change of payment drop down, show the relevant fields below
 $('#payment').change((e) => {
-
-  //todo find a cleaner way to rehide these
+  //quicky and dirty, hide everything, show the target
   $('#credit-card').hide();
   $('#paypal').hide();
   $('#bitcoin').hide();
@@ -106,14 +201,31 @@ $('#payment').change((e) => {
 
 
 
-/*
-If any of the following validation errors exist, prevent the user from submitting the form:
-Name field can't be blank.
-Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
-User must select at least one checkbox under the "Register for Activities" section of the form.
-If the selected payment option is "Credit Card," make sure the user has supplied a Credit Card number, a Zip Code, and a 3 number CVV value before the form can be submitted.
-Credit Card field should only accept a number between 13 and 16 digits.
-The Zip Code field should accept a 5-digit number.
-The CVV should only accept a number that is exactly 3 digits long.
+$('button').click((e) => {
 
-*/
+  //prevent form from actually submitting per default
+  e.preventDefault();
+
+  //do a check to highlight any fields that are not filled out and determine if field is valid
+  //set the t/f values of each criteria
+  let validName = isValidName($name);
+  let validEmail = isValidEmail($email);
+  let validTitle;
+  let validRegistration = isValidRegistration($registration);
+  let validCreditCard;
+
+  if ($title.is(":visible")) {
+    validTitle = isValidTitle($title)
+  }
+  if ($cc_num.is(":visible")) {
+    validCreditCard = isValidCreditCard($cc_num, $zip, $cvv)
+  }
+
+  //then final check before submitting everything
+  if (validEmail && validName && (validTitle || $title.is(":hidden")) && validRegistration && (validCreditCard || $cc_num.is(":hidden"))) {
+    console.log("All fields filled out - Ready to submit form.");
+    $('#register').trigger("reset");
+  } else {
+    console.log(" Cannot Submit Form - Invalid Fields");
+  }
+});
